5eae87b67003e61cf5bd3a7304eb4b6b5791bc7f
Raul <raulespinola@gmail.com>
2020-05-14 17:23:17 -0300

    quite parametro no necesario

--- generators/base-generator.js
+++ generators/base-generator.js
@@ -118,29 +118,29 @@ module.exports = class extends Generator {
 
     generateMainJavaCode(configOptions, templates) {
         const mainJavaRootDir = 'src/main/java/';
-        this._generateCode(configOptions, templates, 'app/', mainJavaRootDir, configOptions.packageFolder, configOptions.proyectoTipo);
+        this._generateCode(configOptions, templates, 'app/', mainJavaRootDir, configOptions.packageFolder);
     }
     
     generateMainResCode(configOptions, templates) {
         const mainResRootDir = 'src/main/resources/';
-        this._generateCode(configOptions, templates, 'app/', mainResRootDir,'', configOptions.proyectoTipo);
+        this._generateCode(configOptions, templates, 'app/', mainResRootDir,'');
     }
 
     generateTestJavaCode(configOptions, templates) {
         const testJavaRootDir = 'src/test/java/';
-        this._generateCode(configOptions, templates, 'app/', testJavaRootDir, configOptions.packageFolder, configOptions.proyectoTipo);
+        this._generateCode(configOptions, templates, 'app/', testJavaRootDir, configOptions.packageFolder);
     }
 
     generateTestResCode(configOptions, templates) {
         const testResRootDir = 'src/test/resources/';
-        this._generateCode(configOptions, templates, 'app/', testResRootDir,'', configOptions.proyectoTipo);
+        this._generateCode(configOptions, templates, 'app/', testResRootDir,'');
     }
 
-    generateFiles(configOptions, templates, srcRoot, baseFolder, proyectoTipo) {
-        this._generateCode(configOptions, templates, srcRoot, baseFolder, '', configOptions.proyectoTipo);
+    generateFiles(configOptions, templates, srcRoot, baseFolder) {
+        this._generateCode(configOptions, templates, srcRoot, baseFolder, '');
     }
 
-    _generateCode(configOptions, templates, srcRoot, baseFolder, packageFolder, proyectoTipo) {
+    _generateCode(configOptions, templates, srcRoot, baseFolder, packageFolder) {
         
       
         templates.forEach(tmpl => {
@@ -152,7 +152,7 @@ module.exports = class extends Generator {
                 );
             } else {
                 this.fs.copyTpl(
-                    this.templatePath(srcRoot + baseFolder + proyectoTipo  +  '/' +  tmpl.src),
+                    this.templatePath(srcRoot + baseFolder  +  '/' +  tmpl.src),
                     this.destinationPath(baseFolder + packageFolder + '/'  + tmpl.dest),
                     configOptions,
                 );
